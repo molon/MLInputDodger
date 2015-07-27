2bfef0214396b54590396d229694a7ee88d10df7
molon <dudl@qq.com>
2015-07-27 09:54:22 +0800

    Initial Commit

---
+++ MLInputDodger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MLInputDodger
+//
+//  Created by molon on 15/7/27.
+//  Copyright (c) 2015年 molon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
